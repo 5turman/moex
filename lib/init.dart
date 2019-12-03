7d9c630940e5a4401030ccb116b25b8b632caaa0
Andrey Babinov <andrey.babinov@gmail.com>
2019-12-03 13:30:36 +0300

    Update packages. Remove timezone data file

--- lib/init.dart
+++ lib/init.dart
@@ -1,25 +1,18 @@
 import 'package:chopper/chopper.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
-import 'package:timezone/timezone.dart';
+import 'package:timezone/data/latest.dart';
 
 import 'build.dart';
 
 Future init() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await _loadTimeZones();
+  initializeTimeZones();
 
   if (Build.debug) {
     _enableHttpLogging();
   }
-}
-
-Future _loadTimeZones() async {
-  const asset = 'res/tz/2019c.tzf';
-  final byteData = await rootBundle.load(asset);
-  initializeDatabase(byteData.buffer.asUint8List());
 }
 
 void _enableHttpLogging() {
